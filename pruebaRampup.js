07af09218bc5f1416963517299950b599cdaf844
Antonio Llamas <antonio@allamas.com>
2023-02-23 09:07:32 +0100

    add exam

---
+++ pruebaRampup.js
@@ -0,0 +1,45 @@
+// ITERACIÓN 1: VARIABLES
+
+// 1.1 Crea una variable developer1 y asignale un nombre cualquiera.
+
+// 1.2.Imprime en consola el texto "El primer desarrollador se llama ****"
+
+// 1.3.Crea una variable developer2 y asigna otro nombre.
+
+// 1.4. Imprime en consola "El segundo desarrollador se llama ****"
+
+// ITERACIÓN 2: CONDICIONALES
+
+// 2.1 Dependiendo de qué desarrollador tenga el nombre más largo se debe imprimir en consola:
+// "El desarrollador con nombre **** es el más largo con ** caracteres".
+
+// Pero si ambos tienen el mismo número de caracteres entonces imprime:
+// "Qué casualidad, ambos nombres tienen ** caracteres".
+
+// ITERACIÓN 3: FUNCIONES Y BUCLES
+
+// Descomenta el siguiente comentario. la función integrada en el lenguaje prompt muestra un popup de entrada de texto para que el usuario escriba un nombre.
+
+// let input = prompt("Escribe un nombre");
+
+// 3.1 Crea una función que devuelva los caracteres de un nombre separados por espacio y en mayusculas.
+
+// 3.2 Crea una función que cuente el número de palabras que tiene el nombre del usuario. Ejemplo si escribe "Antonio Alberto Jesús" devolverá 3.
+
+// 3.3 Crea una tercera función llamada printUserDetails que haga uso de las dos funciones anteriores para imprimir en consola la siguiente salida:
+
+// Si por ejemplo el usuario introduce en el prompt el input "Antonio Alberto Jesús", en la consola se imprimira tal cual el siguiente comentario:
+
+/*
+
+DETALLES para 'Antonio Alberto Jesus':
+
+Numero de palabras 3:
+- A N T O N I O (7 caracteres)
+- A L B E R T O (7 caracteres)
+- J E S U S (5 caracteres) 
+
+*/
+
+// Happy coding!
+// www.google.com
